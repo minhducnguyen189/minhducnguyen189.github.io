9c2ba28df30e03033d40e964da2b4da897fcaee5
minhducnguyen189 <minhducnguyen189@gmail.com>
2021-10-17 12:28:49 +0700

    test deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
 echo 'Building source code'
-echo -ne '[](0%)\r'
 sudo jekyll build --config=_config.yml
-echo -ne '[##########](10%)\r'
 echo 'Setting permission for generated source'
 sudo chmod 777 _site -R
-echo -ne '[####################](20%)\r'
 echo 'copying generated source'
 cp -R -T _site /home/duc/projects/minhducnguyen189.github.io -f
-echo -ne '[########################################](40%)\r'
 echo 'Navigate to minhducnguyen189.github.io'
 cd /home/duc/projects/minhducnguyen189.github.io
 echo 'Deploying'
 echo "Enter commit message: "
 read message
-echo -ne '[############################################################](60%)\r'
 git add .
-echo -ne '[################################################################################](80%)\r'
 git commit -m "$message"
-echo -ne '[##########################################################################################](90%)\r'
 git push
-echo -ne '[####################################################################################################](100%)\r'
-echo -ne '\n'
+echo 'the deployment has just been done'
 
